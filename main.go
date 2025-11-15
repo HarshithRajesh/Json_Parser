abe62852805071043c6df90155802eb0b80e28ae
harshith <harshithr2004@gmail.com>
2025-11-15 23:19:55 +0530

    adding tokens

--- main.go
+++ main.go
@@ -4,6 +4,32 @@ import (
 	"fmt"
 )
 
+const (
+	// Token/character we don't know about
+	Illegal Type = "ILLEGAL"
+
+	// End of file
+	EOF Type = "EOF"
+
+	// Literals
+	String Type = "STRING"
+	Number Type = "NUMBER"
+
+	// The six structural tokens
+	LeftBrace    Type = "{"
+	RightBrace   Type = "}"
+	LeftBracket  Type = "["
+	RightBracket Type = "]"
+	Comma        Type = ","
+	Colon        Type = ":"
+
+	// Values
+	True  Type = "TRUE"
+	False Type = "FALSE"
+	Null  Type = "NULL"
+)
+
 func main() {
 	fmt.Println("Welcome to JSON Parser!!!")
+
 }
